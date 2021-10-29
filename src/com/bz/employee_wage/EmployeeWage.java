fd633795cb42b6292e449d9c332117556cc3fc18
mudable <mudableshilpa@gmail.com>
2021-10-29 13:42:32 +0530

    Added : UC7 to refactor the code using Oops concept

--- src/com/bz/employee_wage/EmployeeWage.java
+++ src/com/bz/employee_wage/EmployeeWage.java
@@ -1,37 +1,56 @@
 package com.bz.employee_wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
+	final static int IS_PART_TIME = 1;
+	final static int IS_FULL_TIME = 2;
+	static int EMP_RATE_PER_HOUR = 100;
+	static int NO_OF_WORKING_DAYS = 20;
+	static int MAX_HRS_IN_THE_MONTH = 10;
+	int empHrs;
+	int totalEmpHrs = 0;
+	int totalWorkingDays = 0;
+	int totalEmpWage = 0;
+
 	/*
 	 * calculate employee present or absent using random function. calculating
 	 * employee wage. Added part time to calculate employee wage. Applied switch
 	 * statement.
 	 */
-	public static void main(String[] args) {
-		int RATE_PER_HR = 20;
-		int FULL_DAY_HR = 8;
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 0;
-		int NO_OF_WORKING_DAYS = 20;
-		int totalEmpWage = 0;
-		int empHrs = 0;
-		System.out.println("Welcome to EmPloyee Wage Program");
-		for (int i = 0; i < NO_OF_WORKING_DAYS; i++) {
-			int empCheck = (int) (Math.random() * 10) % 2;
+	public void calculateEmpWage() {
+		while (totalEmpHrs <= MAX_HRS_IN_THE_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			Random random = new Random();
+			int empCheck = random.nextInt(3);
 			switch (empCheck) {
 			case IS_PART_TIME:
-				empHrs = 4;
+				empHrs = 8;
 				break;
 			case IS_FULL_TIME:
-				empHrs = 8;
+				empHrs = 12;
 				break;
 			default:
 				empHrs = 0;
 				break;
 			}
-			int empWage = empHrs * RATE_PER_HR;
-			totalEmpWage = totalEmpWage + empWage;
-			System.out.println("Emp Wage: " + empWage);
-			System.out.println("Total Emp Wage:" + totalEmpWage);
 		}
+	}
+
+	public int empHour() {
+		System.out.println("Days#" + totalWorkingDays + "EmpHrs :" + empHrs);
+		return totalEmpHrs += empHrs;
+	}
+
+	public int totalEmpWage() {
+		int value = empHour();
+		return totalEmpWage = value * EMP_RATE_PER_HOUR;
+	}
+
+	public static void main(String[] args) {
+		EmployeeWage obj = new EmployeeWage();
+		obj.calculateEmpWage();
+		obj.totalEmpWage();
+		System.out.println("Total Emp Wage:" + obj.totalEmpWage);
 	}
 }
